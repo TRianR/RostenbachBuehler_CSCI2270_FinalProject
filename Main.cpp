1153edbf40b70933d75dc299b5c50379cfe02443
user <jobu4649@colorado.edu>
2015-04-21 15:16:51 -0600

    update Main file

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,53 @@
+/*
+ * Josh Buehler
+ * Tyler Rostenbach
+ * text based dungeon crawler
+ */
 
+#include "Game.h"
+#include "Game.cpp"
+#include <iostream>
+using namespace std;
+
+Game G= Game();
+
+int main()
+{
+	cout<<"Welcome to House of Frights"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"There are three characters to play"<<endl;
+	cout<<"Logan is a footbal player for the varsity team. He is a little on the shy side, but is nice to those around him. Logan plays defensive tackle, and yes he lifts."<<endl;
+	cout<<"Kari is head of the cheersquad, she and logan are dating. She is a little stuck up, but always supports logan. She always claims the top of the pyramid."<<endl;
+	cout<<"Jackson has been friends with Kari and logan since first grade. He's a pretty average guy, who doesn't take anything too seriously. Jackson can keep up with Kari in a run, but works out with Logan."<<endl;
+	
+	bool happy=false;
+	string cont;
+	string choice;
+	while(happy == false)
+	{
+		cout<<"Who will you play as?"<<endl;
+		cout<<"1. Logan"<<endl;
+		cout<<"2. Kari"<<endl;
+		cout<<"3. Jackson"<<endl;
+		cin>>choice;
+		setPlayer(stoi(choice));
+		preChoiceStatus();
+		cout<<endl;
+		cout<<endl;
+		cout<<endl;
+		cout<<"Are you happy with your choice?"<<endl;
+		cout<<endl;
+		cout<<"           y/n"<<endl;
+		cin<<cont;
+		if(cont=='y')
+		{
+			happy=true;
+		}
+		else
+		{
+			happy=false;
+		}
+		
+	}
+}
